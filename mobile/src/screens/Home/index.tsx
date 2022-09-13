8e9598e73d808c40b7b6eb1967d21ebad468ba7f
Bruno Sajermann <sajermannbruno@gmail.com>
2022-09-13 20:16:37 -0300

    Video 2 Complete

---
+++ mobile/src/screens/Home/index.tsx
@@ -0,0 +1,29 @@
+import { View, FlatList, Image } from "react-native";
+import { styles } from "./styles";
+
+import logoImg from "../../assets/logo-nlw-esports.png";
+import { Heading } from "../../components/Heading";
+import { GameCard } from "../../components/GameCard";
+
+import { GAMES } from "../../utils/games";
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Image source={logoImg} style={styles.logo} />
+
+      <Heading
+        title="Encontre seu duo"
+        subtitle="Selecione o game que deseja jogar..."
+      />
+      <FlatList
+        data={GAMES}
+        keyExtractor={(item) => item.id}
+        renderItem={({item}) => <GameCard data={item} />}
+        horizontal
+        showsHorizontalScrollIndicator
+        contentContainerStyle={styles.contentList}
+      />
+    </View>
+  );
+}
